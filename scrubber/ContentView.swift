93fddd5538d58ac192933b87f2e497fdafa4e050
Richard <richard.verheyen@gmail.com>
2024-02-11 14:45:29 +1100

    Initial Commit

---
+++ scrubber/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  scrubber
+//
+//  Created by Richard Verheyen on 11/2/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ scrubber/scrubberApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrubberApp.swift
+//  scrubber
+//
+//  Created by Richard Verheyen on 11/2/2024.
+//
+
+import SwiftUI
+
+@main
+struct scrubberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
